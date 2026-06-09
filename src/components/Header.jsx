8f79892ebe66ddc866d18fc1660eb062e02b5ada
SG HARSHAA <harshaavardhan8@gmail.com>
2026-06-09 16:32:39 +0530

    usereducer-todos!

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,12 @@ function Header({ addTodo }) {
 
   const handleSubmit = () => {
     if (name.trim() === "") return;
-    addTodo({ name, desc });
+
+    addTodo({
+      name,
+      desc,
+    });
+
     setName("");
     setDesc("");
   };
@@ -14,20 +19,35 @@ function Header({ addTodo }) {
   return (
     <div className="header">
       <h1 className="todo-title">Todo App!!</h1>
-      <div className="todo-input-box">
+
+      <div
+        className="todo-input-box"
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "24px",
+          marginTop: "24px",
+        }}
+      >
         <input
           type="text"
           placeholder="Todo Name"
           value={name}
           onChange={(e) => setName(e.target.value)}
         />
+
         <input
           type="text"
           placeholder="Todo Description"
           value={desc}
           onChange={(e) => setDesc(e.target.value)}
         />
-        <button className="add-btn" onClick={handleSubmit}>
+
+        <button
+          className="add-btn"
+          onClick={handleSubmit}
+          style={{ width: "100%" }}
+        >
           Add Todo
         </button>
       </div>
